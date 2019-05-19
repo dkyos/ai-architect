cee1909bb4b927b9e85db3bf2fa882df31b6b52f
dk77.yun <dk77.yun@samsung.com>
2019-05-19 13:32:37 +0900

    update for most_similar demo
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- ai_architect/api/word2vec_api.py
+++ ai_architect/api/word2vec_api.py
@@ -13,23 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ******************************************************************************
+import pickle
+from os import makedirs, path, sys
+
+import numpy as np
 
 from ai_architect.api.abstract_api import AbstractApi
+from ai_architect.models.wd2vec import WD2vec
+from ai_architect import LIBRARY_OUT
+from ai_architect.utils.generic import pad_sentences
+from ai_architect.utils.io import download_unlicensed_file
+
 
 
 class Word2VecApi(AbstractApi):
     """
     Word2Vec API
     """
+    model_dir = str(LIBRARY_OUT / 'wd2vec-pretrained')
+    pretrained_model = path.join(model_dir, 'wd2vec_v0.h5')
+
     def __init__(self):
+        print("Word2VecApi: init")
         self.model = None
 
     def load_model(self):
         """
         Load Word2Vec model
         """
-        print("Load Word2Vec model")
-        #######
+        print("Word2VecApi: load_model")
+        self.model = WD2vec.load(self.pretrained_model)
 
     def inference(self, doc):
         """
@@ -42,10 +55,10 @@ class Word2VecApi(AbstractApi):
             XXXX
 
         """
-
-        print("Inference Word2Vec model")
+        print("Word2VecApi: inference")
         print("Doc: " + doc)
-        ret = {'doc_text': doc, 'annotation_set': []}
+        ret_str = str(self.model.most_similar(doc))
+        ret = {'doc_text': ret_str , 'annotation_set': []}
         spans = []
         available_tags = set()
         ret['annotation_set'] = list(available_tags)
@@ -55,4 +68,3 @@ class Word2VecApi(AbstractApi):
         print({"doc": ret, 'type': 'high_level'})
 
         return {'doc': ret, 'type': 'high_level'}
-        #return self.model.parse(doc)
